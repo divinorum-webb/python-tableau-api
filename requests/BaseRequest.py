d0332ae090fa52ae5bcab0d558ab9fa932240637
Elliott <elliott.stam@gmail.com>
2019-05-14 15:30:28 -0700

    added BaseRequest class

---
+++ requests/BaseRequest.py
@@ -0,0 +1,24 @@
+class BaseRequest:
+    """
+    Base request for generating API request URLs to Tableau Server.
+
+    :param ts_connection:       The Tableau Server connection object.
+    :type ts_connection:        class
+    """
+    def __init__(self,
+                 ts_connection):
+
+        self._connection = ts_connection
+        self._request_body = {}
+
+    @staticmethod
+    def _get_parameters_dict(param_keys, param_values):
+        params_dict = {}
+        for i, key in enumerate(param_keys):
+            if param_values[i]:
+                params_dict.update({key: param_values[i]})
+        return params_dict
+
+    def _invalid_parameter_exception(self):
+        raise Exception("{} received an invalid combination of parameters: \n{}".format(self.__class__.__name__,
+                                                                                        vars(self)))
