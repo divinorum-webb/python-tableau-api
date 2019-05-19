7bca1f6cffa9e84a42d6a2198355fe9f99091bc0
Elliott Stam <elliott.stam@gmail.com>
2019-05-18 22:17:39 -0700

    added class: UpdateWorkbookRequest

---
+++ requests/UpdateWorkbookRequest.py
@@ -0,0 +1,76 @@
+class UpdateWorkbookRequest(BaseRequest):
+    """
+    Update site request for generating API request URLs to Tableau Server.
+
+    :param ts_connection:       The Tableau Server connection object.
+    :type ts_connection:        class
+    :param show_tabs_flag:
+    :type show_tabs_flag:
+    :param project_id:
+    :type project_id:
+    :param owner_id:
+    :type owner_id:
+    """
+    def __init__(self,
+                 ts_connection,
+                 show_tabs_flag=False,
+                 project_id=None,
+                 owner_id=None):
+
+        super().__init__(ts_connection)
+        self._show_tabs_flag = show_tabs_flag
+        self._project_id = project_id
+        self._owner_id = owner_id
+        self.base_update_workbook_request
+
+    @property
+    def optional_workbook_param_keys(self):
+        return ['showTabs']
+
+    @property
+    def optional_project_param_keys(self):
+        return ['id']
+
+    @property
+    def optional_owner_param_keys(self):
+        return ['id']
+
+    @property
+    def optional_workbook_param_values(self):
+        return ['true' if self._show_tabs_flag == True else 'false' if self._show_tabs_flag == False else None]
+
+    @property
+    def optional_project_param_values(self):
+        return [self._project_id]
+
+    @property
+    def optional_owner_param_values(self):
+        return [self._owner_id]
+
+    @property
+    def base_update_workbook_request(self):
+        self._request_body.update({'workbook': {}})
+        return self._request_body
+
+    @property
+    def modified_update_workbook_request(self):
+        if any(self.optional_workbook_param_values):
+            self._request_body['workbook'].update(
+                self._get_parameters_dict(self.optional_workbook_param_keys,
+                                          self.optional_workbook_param_values))
+
+        if any(self.optional_project_param_values):
+            self._request_body['workbook'].update({'project': {}})
+            self._request_body['workbook']['project'].update(
+                self._get_parameters_dict(self.optional_project_param_keys,
+                                          self.optional_project_param_values))
+
+        if any(self.optional_owner_param_values):
+            self._request_body['workbook'].update({'owner': {}})
+            self._request_body['workbook']['owner'].update(
+                self._get_parameters_dict(self.optional_owner_param_keys,
+                                          self.optional_owner_param_values))
+        return self._request_body
+
+    def get_request(self):
+        return self.modified_update_workbook_request
