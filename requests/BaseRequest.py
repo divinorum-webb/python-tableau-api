4498f361f5572ef0731e17b5e34926415bf31fab
Elliott Stam <elliott.stam@gmail.com>
2019-06-06 23:34:31 -0700

    Updated docstring for class: BaseRequest

--- requests/BaseRequest.py
+++ requests/BaseRequest.py
@@ -1,6 +1,6 @@
 class BaseRequest:
     """
-    Base request for generating API request URLs to Tableau Server.
+    Base request for issuing API requests to Tableau Server.
 
     :param ts_connection:       The Tableau Server connection object.
     :type ts_connection:        class
