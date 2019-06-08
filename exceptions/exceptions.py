d1986d51b8eccf5764586aba9ab77463ffa774a0
Elliott Stam <elliott.stam@gmail.com>
2019-06-08 16:52:14 -0700

    Added class: BaseError(Exception) Renamed InvalidParameterException to InvalidParameter(BaseError) Renamed InvalidWorkbookTypeException to InvalidWorkbookType(BaseError) Updated docstrings for exception classes

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,6 +1,13 @@
-class InvalidParameterException(Error):
+class BaseError(Exception):
+    """Base class for additional exceptions"""
     pass
 
 
-class InvalidWorkbookTypeException(Error):
+class InvalidParameter(BaseError):
+    """Raised when an invalid set of parameters are passed to an Endpoint or Request class"""
+    pass
+
+
+class InvalidWorkbookType(BaseError):
+    """Raised when attempting to call publish_workbook() with an invalid file type (valid types are twbx or twb)"""
     pass
