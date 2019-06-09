0f793ec56b1436097b4e669dbdf1c2d33a9fa388
Elliott Stam <elliott.stam@gmail.com>
2019-06-08 17:16:29 -0700

    Updated class: WorkbookEndpoint Added parameter update_workbook Updated docstring

--- endpoints/WorkbookEndpoint.py
+++ endpoints/WorkbookEndpoint.py
@@ -10,6 +10,8 @@ class WorkbookEndpoint(BaseEndpoint):
     :type query_workbook:                       boolean
     :param publish_workbook:                    Boolean flag; True if publishing a specific workbook, False otherwise.
     :type publish_workbook:                     boolean
+    :param update_workbook:                     Boolean flag; True if updating a specific workbook, False otherwise.
+    :type update_workbook:                      boolean
     :param delete_workbook:                     Boolean flag; True if deleting a specific workbook, False otherwise.
     :type delete_workbook:                      boolean
     :param workbook_id:                         The workbook ID.
@@ -53,6 +55,7 @@ class WorkbookEndpoint(BaseEndpoint):
                  query_workbooks=False,
                  query_workbook=False,
                  publish_workbook=False,
+                 update_workbook=False,
                  delete_workbook=False,
                  workbook_id=None,
                  view_id=None,
@@ -74,6 +77,7 @@ class WorkbookEndpoint(BaseEndpoint):
         self._query_workbooks = query_workbooks
         self._query_workbook = query_workbook
         self._publish_workbook = publish_workbook
+        self._update_workbook = update_workbook
         self._delete_workbook = delete_workbook
         self._workbook_id = workbook_id
         self._view_id = view_id
@@ -150,6 +154,8 @@ class WorkbookEndpoint(BaseEndpoint):
             if self._query_workbook:
                 url = self.base_workbook_id_url
             elif self._delete_workbook:
+                url = self.base_workbook_id_url
+            elif self._update_workbook and not (self._delete_workbook or self._publish_workbook):
                 url = self.base_workbook_id_url
             elif self._add_tags:
                 url = self.base_workbook_tags_url
